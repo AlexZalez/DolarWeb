51b8561a0e4949fe11e5a28abd70cafd6135b45b
AlexZalez <remixsicool@gmail.com>
2023-10-17 00:18:12 -0400

    Charge state, calc dolar

--- src/components/DolarWeb.vue
+++ src/components/DolarWeb.vue
@@ -1,17 +1,36 @@
 <script setup>
     import axios from 'axios';
-    import {ref, onMounted} from 'vue';
+    import {ref, onMounted, computed} from 'vue';
 
     const precio = ref('');
+    const isLoading = ref(true)
+    const setIsLoading = (state) => {
+            isLoading.value = state;
+    }
+    
+    const dolarON = ref(false);
+    const dolarIN = ref(null);
+
+    const bolivarON = ref(false);
+    const bolivarVAL = computed(()=>{
+        let newVal = precio.value * dolarIN.value;
+        return `${newVal.toFixed(2)} Bs`;
+    })
 
     function valor() {
-        axios.get('https://pydolarvenezuela-api.vercel.app/api/v1/dollar/bcv').then(res =>{
+        axios.get('https://pydolarvenezuela-api.vercel.app/api/v1/dollar/bcv')
+        .then(res =>{
             console.log(res.data.monitors.usd.price);
             precio.value = res.data.monitors.usd.price;
+        })
+        .catch(error => {
+
+        })
+        .finally(() => {
+            setIsLoading(false)
         });
         return
     }
-
     onMounted(()=>{
         valor();
     })
@@ -19,14 +38,44 @@
 
 <template>
     <div class="h-screen flex justify-center items-center">
-        <div class="flex flex-col bg-green-800 rounded-3xl px-6 py-3 text-white">
+        <div v-if="isLoading" class="flex flex-col bg-green-800 rounded-3xl px-6 py-3 text-white">
+            <div>
+                CARGANDO
+            </div>
+        </div>
+        <div v-else class="flex flex-col bg-green-800 rounded-3xl px-6 py-3 text-white">
             <div class=" font-bold text-center">Dolar BCV</div>
             <div class="flex">
-                <div class="">1$ = </div>
-                <div class="">{{ ` ${precio.substring(0,5)}BsS` }}</div>
+                <div class="group">
+                    <div @click="dolarON = !dolarON" v-if="!dolarON" class="flex">
+                        <div class="group-hover:bg-white/50 px-1 py-0.5 rounded-lg cursor-pointer">1$</div>
+                        <div class="mr-1">=</div>
+                    </div>
+                    
+                    <div v-else class="flex">
+                        <div class="flex">
+                            <input type="text" v-model="dolarIN" class="myIN">
+                            <div>$</div>
+                        </div>
+                        <div class="mx-1">=</div>
+                    </div>
+                </div>
+                <div class="">
+                    <div @click="bolivarON = !bolivarON" v-if="dolarON">{{ bolivarVAL }}</div>
+                    <div v-else>{{ `${precio} Bs` }}</div>
+                </div>
             </div>
+
         </div>
     </div>
 </template>
+
+<style>
+
+    .myIN{
+        @apply bg-transparent outline-none w-6
+    }
+
+</style>
 
 
